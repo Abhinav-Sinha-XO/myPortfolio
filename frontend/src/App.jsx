1ead3c2acf0f1bdd4e649ed2be11bad9941f5402
Abhinav Sinha <www.abhinav9090@gmail.com>
2025-02-27 01:18:57 +0530

    navbar -N

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import About from "./components/About";
--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5001/api/contacts", {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/contacts`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
