c4dfdd3be2f2ca5383c7486b0f731d1597659bed
Abhinav Sinha <www.abhinav9090@gmail.com>
2025-02-27 01:38:32 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5174", // Your frontend URL
+    origin: process.env.FRONTEND_URL, // Use environment variable for frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
